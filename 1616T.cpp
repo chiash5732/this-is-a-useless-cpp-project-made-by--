49b5b7bfe3a61e8f7ef05ac546db97c73e7f4553
chiash5732 <chiash5732@gmail.com>
2020-09-21 20:42:54 +0800

    tweak 1616T.cpp

--- 1616T.cpp
+++ 1616T.cpp
@@ -24,31 +24,29 @@ int main()
             cout << "\n";
         }
     }
-    for (int j = 0; j <= 15; j++) //input group
-    {
-        for (int i = 0; i <= 15; i++)
-        {
-            cout << "input Group " << j << " No." << i << "(if input>255 to next group):";
-            cin >> input;
-            if (input > 255)
-            {
-                break;
-            }
-            else
-            {
-                G[j][i] = input;
-            }
-        }
-    }
+    // for (int j = 0; j <= 15; j++) //input group
+    // {
+    //     for (int i = 0; i <= 15; i++)
+    //     {
+    //         cout << "input Group " << j << " No." << i << "(if input>255 to next group):";
+    //         cin >> input;
+    //         if (input > 255)
+    //         {
+    //             break;
+    //         }
+    //         else
+    //         {
+    //             G[j][i] = input;
+    //         }
+    //     }
+    // }
     int NL[257], FinalC, Check[15], NCN, whilecounter, totalruncounter;
     int min = 0, max = 15;         //set rand range
     for (int i = 0; i <= 255; i++) //fill num
     {
-        if (NL[i] == 0)
-        {
-            srand(time(NULL));
-            NL[i] = rand() % (max - min + 1) + min;
-        }
+        srand(time(NULL) * 1000);
+        NL[i] = rand() % (max - min + 1) + min;
+        // cout << "fill " << i <<NL[i];
     }
     int x, y;
     do
@@ -58,33 +56,27 @@ int main()
         y = 16;
         for (int k = 0; k < 16; k++)
         {
-            for (int i = x; i < y; i++)
+            for (int j = x; j < y; j++)
             {
-                for (int j = x; j < y; j++)
+                if (Check[(NL[j])] == 0)
                 {
-                    if (i == j)
-                    {
-                        continue;
-                    }
-                    else if (Check[(NL[j])] == 0)
-                    {
-                        Check[(NL[j])] = 1;
-                    }
-                    else if (Check[(NL[j])] == 1)
-                    {
-                        do
-                        {
-                            srand(time(NULL));
-                            NCN = rand() % (max - min + 1) + min;
-                        } while (Check[NCN] == 0);
-                        NL[j] = NCN;
-                        FinalC = 1;
-                    }
+                    Check[(NL[j])] = 1;
                 }
-                for (int j = 0; j < 16; j++)
+                else if (Check[(NL[j])] == 1)
                 {
-                    Check[j] = 0;
+                    NCN = 0;
+                    do
+                    {
+                        NCN++;
+                    } while (Check[NCN] == 0);
+                    NL[j] = NCN;
+                    FinalC = 1;
+                    cout << "1";
                 }
+            }
+            for (int j = 0; j < 16; j++)
+            {
+                Check[j] = 0;
             }
             x += 16;
             y += 16;
@@ -94,78 +86,68 @@ int main()
         y = 256;
         for (int k = 0; k < 16; k++)
         {
-            for (int i = x; i < y; i += 16)
+            for (int j = x; j < y; j += 16)
             {
-                for (int j = x; j < y; j++)
+                if (Check[(NL[j])] == 0)
                 {
-                    if (i == j)
-                    {
-                        continue;
-                    }
-                    else if (Check[(NL[j])] == 0)
-                    {
-                        Check[(NL[j])] = 1;
-                    }
-                    else if (Check[(NL[j])] == 1)
-                    {
-                        do
-                        {
-                            srand(time(NULL));
-                            NCN = rand() % (max - min + 1) + min;
-                        } while (Check[NCN] == 0);
-                        NL[j] = NCN;
-                        FinalC = 1;
-                    }
+                    Check[(NL[j])] = 1;
                 }
-                for (int j = 0; j < 16; j++)
+                else if (Check[(NL[j])] == 1)
                 {
-                    Check[j] = 0;
+                    NCN = 0;
+                    do
+                    {
+                        NCN++;
+                    } while (Check[NCN] == 0);
+                    NL[j] = NCN;
+                    FinalC = 2;
+                    cout << "2";
                 }
+            }
+            for (int j = 0; j < 16; j++)
+            {
+                Check[j] = 0;
             }
             x++;
         }
 
-        for (int k = 0; k <= 15; k++)
-        {
-            for (int i = 0; i < 16; i++)
-            {
-                for (int j = 0; j < 16; j++)
-                {
-                    if (i == j)
-                    {
-                        continue;
-                    }
-                    else if (Check[NL[(G[k][j])]] == 0 && G[k][i] != 257 && G[k][j] != 257)
-                    {
-                        Check[NL[(G[k][j])]] = 1;
-                    }
-                    else if (Check[NL[(G[k][j])]] == 1)
-                    {
-                        do
-                        {
-                            srand(time(NULL));
-                            NCN = rand() % (max - min + 1) + min;
-                        } while (Check[NCN] == 0);
-                        NL[j] = NCN;
-                        FinalC = 1;
-                    }
-                }
-                for (int j = 0; j < 16; j++)
-                {
-                    Check[j] = 0;
-                }
-            }
-        }
+        // for (int k = 0; k <= 15; k++)
+        // {
+        //     for (int j = 0; j <= 15; j++)
+        //     {
+        //         if (G[k][j] == 257)
+        //         {
+        //             continue;
+        //         }
+        //         else if (Check[(NL[(G[k][j])])] == 0)
+        //         {
+        //             Check[(NL[(G[k][j])])] = 1;
+        //         }
+        //         else if (Check[(NL[(G[k][j])])] == 1)
+        //         {
+        //             NCN = 0;
+        //             do
+        //             {
+        //                 NCN++;
+        //             } while (Check[NCN] == 0);
+        //             NL[j] = NCN;
+        //             FinalC = 3;
+        //         }
+        //     }
+        //     for (int j = 0; j < 16; j++)
+        //     {
+        //         Check[j] = 0;
+        //     }
+        // }
         cout << "`";
         whilecounter++;
-        if (whilecounter >= 2000)
+        if (whilecounter >= 1000)
         {
             system("cls");
             whilecounter = 0;
             totalruncounter++;
         }
-        system("pause");
-    } while (FinalC = 1);
+    } while (FinalC >= 1);
     cout << endl;
     for (int i = 0; i < 256; i++)
     {
@@ -175,7 +157,7 @@ int main()
             cout << "\n";
         }
     }
-    cout << "\ntotalrun:" << totalruncounter * 2000 << endl;
+    cout << "\ntotalrun:" << totalruncounter * 1000 << endl;
     system("pause");
     return 0;
 }
